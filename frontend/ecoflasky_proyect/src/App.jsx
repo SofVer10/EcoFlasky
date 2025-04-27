c192c3f872b7f7066c1558842f4c7d5e39cafd5a
sandoval <20200300@ricaldone.edu.sv>
2025-04-27 17:23:57 -0600

    trabaje en contactanos

--- frontend/ecoflasky_proyect/src/App.jsx
+++ frontend/ecoflasky_proyect/src/App.jsx
@@ -1,27 +1,32 @@
 
 import './App.css'
 import { BrowserRouter as Router, Routes, Route }
-from "react-router"
+  from "react-router"
 
 import Products from './pages/products'
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
+import Contactanos from './pages/contacts';
+
 
 function App() {
-  
+
 
   return (
     <>
-     <Router>
-        <Nav/>
-          <Routes>
-            <Route path="/" element={<Products/>} />
-          </Routes>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/contactanos" element={<Contactanos />} />
+        </Routes>
       </Router>
-      <Footer/>
+      <Footer />
 
     </>
   )
+
+
 }
 
 export default App
