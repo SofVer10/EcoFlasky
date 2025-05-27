a27f20e4a9f2fd646d4d428a2e643a6041e7d147
Aarón García <aaron.edgardo.garcia@gmail.com>
2025-05-27 13:53:18 -0600

    Para que sirva en render

--- backend/src/sources/CategoryRoutes.js
+++ backend/src/sources/CategoryRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import categoryController from "../controllers/CategoryController.js"
+import categoryController from "../controllers/categoryController.js"
 
 const router = express.Router();
 router.route("/")
