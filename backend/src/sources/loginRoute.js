e017b0735abadcb24848c701abfed1c4495e4c2c
Aarón García <aaron.edgardo.garcia@gmail.com>
2025-05-27 14:00:16 -0600

    Para que sirva en render

--- backend/src/sources/loginRoute.js
+++ backend/src/sources/loginRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import loginController from "../Controllers/loginController.js"
+import loginController from "../controllers/loginController.js"
 
 const router = express.Router();
 
